437db27b666e7013457e6640faf9a6c722b04d2a
rodoggx <mikerodolph@yahoo.com>
2016-08-17 17:37:38 -0400

    first commit

---
+++ app/src/main/java/com/example/activityresult/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.activityresult;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_CONTACT_REQUEST = 10;
+    private static final String TAG = "MainActivityTAG_";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doMagic(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Uri contactUri = data.getData();
+
+                Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+
+                if (cursor != null) {
+                    cursor.moveToFirst();
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                    Log.d(TAG, "onActivityResult: " + name + " " + number);
+
+                    cursor.close();
+                }
+
+            }
+        }
+    }
+}
